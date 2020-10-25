09f78c7513f551277de9daef7296b240f8edd438
Afdallah <afdallah.war@gmail.com>
2020-10-25 20:41:51 +0700

    Update button comp

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,16 +3,18 @@ import classNames from 'classnames'
 import { ImSpinner2 } from 'react-icons/im'
 import Icon from './SvgIcons.js'
 
-function Button({ variant, children, type, isSpinning, ...rest }) {
+function Button({ icon, variant, children, type, isSpinning, className, ...rest }) {
 
   const btnClass = classNames({
     button: true,
-    [`button--${variant}`]: variant
+    [`button--${variant}`]: variant,
+    ...className
   })
 
   return (
     <button className={btnClass} type={type} {...rest}>
-      <Icon type={variant} />
+      {!icon ? <Icon type={variant} /> : {icon}}
+
 
       {isSpinning && (
         <div className="button__spinner">
