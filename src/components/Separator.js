49db03ceb48679010b871e777932a8ba3aeacfbf
Afdallah <afdallah.war@gmail.com>
2020-10-25 11:11:28 +0700

    Separator component

---
+++ src/components/Separator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Separator({ text }) {
+  return (
+    <div className="separator">
+      <span>{text}</span>
+    </div>
+  );
+}
+
+export default Separator;
