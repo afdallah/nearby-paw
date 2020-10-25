15af765c528dbb355c08bfca1fafc086806420b6
Afdallah <afdallah.war@gmail.com>
2020-10-25 11:11:33 +0700

    Add logout function

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,17 @@
-import React from 'react'
-import iconComment from '../assets/images/comment.svg'
-import logo from '../assets/images/logo/logo-pet.svg'
-import bell from '../assets/images/bell.svg'
+import React from 'react';
+import { withRouter } from 'react-router-dom';
 
-function Navbar() {
+// Load files
+import iconComment from '../assets/images/comment.svg';
+import logo from '../assets/images/logo/logo-pet.svg';
+import bell from '../assets/images/bell.svg';
+
+function Navbar({ history }) {
+  const handleLogout = () => {
+    localStorage.removeItem('access_token');
+    history.push('/signin');
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar__container">
@@ -23,11 +31,13 @@ function Navbar() {
                 alt="avatar"
               />
             </div>
+
+            <div className="clickable" onClick={handleLogout}>Logout</div>
           </div>
         </div>
       </div>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default withRouter(Navbar);
